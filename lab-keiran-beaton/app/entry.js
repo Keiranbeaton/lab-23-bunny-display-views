fd72ec5b07be6395769e94d7668b8d88d2fab14c
keiran beaton <keiran beaton>
2016-08-22 20:28:39 -0700

    added webpack config, eslintrc, gitignore, package.json and entry.js

---
+++ lab-keiran-beaton/app/entry.js
@@ -0,0 +1,46 @@
+'use strict';
+
+require('!!file?name=[name].[ext]!./html/index/html');
+require('./scss/base.scss');
+
+const angular = require('angular');
+const angular_route = require('angular-route');
+
+var app = angular.module('imageApp', [angular_route]);
+
+app.run(['$rootScope', function($rootScope) {
+  $rootScope.imageData = require('./data/images.js');
+  $rootScope.errorMessage = 'Error trying to access unknown page';
+}]);
+
+app.config(['$routeProvider', function($route) {
+  $route
+    .when('/', {
+      templateUrl: '/app/view/home/home.html',
+      controller: 'HomeController',
+      controllerAs: 'hc'
+    })
+    .when('/thumbnail', {
+      templateUrl: '/app/view/thumbnail/thumbnail.html',
+      controller: 'ThumbnailController',
+      controllerAs: 'tc'
+    })
+    .when('/error', {
+      templateUrl: '/app/view/error/error.html',
+      controller: 'ErrorController',
+      controllerAs: 'ec'
+    })
+    .when('/fullsize/:id', {
+      templateUrl: '/app/view/fullsize/fullsize.html',
+      controller: 'FullsizeController',
+      controllerAs: 'fc'
+    })
+    .otherwise({
+      redirectTo: '/error'
+    });
+}]);
+
+require('./view/home');
+require('./view/thumbnail');
+require('./view/fullsize');
+require('./view/error');
